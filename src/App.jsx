9d242f39e297c844e002785d04eaa785007491d3
sudheer0071 <sudheer1614@gmail.com>
2024-02-17 18:55:29 +0530

    adding some imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Assgnment1 } from './assignments/Assgnment1'
+import { Assgnment2 } from './assignments/Assgnment2'
+import { Assgnment3 } from './assignments/Assgnment3'
+import { Assgnment4 } from './assignments/Assgnment4'
+import { Assgnment5 } from './assignments/Assgnment5'
 
 function App() {
   const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+    <Assgnment1/>
+    <Assgnment2/>
+    <Assgnment3/>
+    <Assgnment4/>
+    <Assgnment5/>
     </>
   )
 }
