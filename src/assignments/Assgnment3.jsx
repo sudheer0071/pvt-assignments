6b91344b2f6ae36441f3c4b23056fdba3d273712
sudheer0071 <sudheer1614@gmail.com>
2024-02-18 19:22:41 +0530

    q3 done

--- src/assignments/Assgnment3.jsx
+++ src/assignments/Assgnment3.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react"
+
 export function Assgnment3(){
-  return <button>sadfj</button>
-}
+
+  const[num,setNum] = useState('')
+  const [para, setPara] = useState('')
+
+  const handleclick = ()=>{
+    setPara("Thi is para is generated")
+  }
+
+
+  function generatePara(){
+      const words = ['This','is','Randomly','Generated','paragraph','Which','Does','not','makes','sense']
+
+      
+
+  }
+  return <div> 
+   <h2>Para Generator</h2>
+   <input type="text" placeholder="Enter Number of words" onChange={(e)=>{setNum(e.target.value)}} />
+   <button onClick={handleclick}>Generator</button> 
+   <div className="para">{para}</div>
+  </div>
+}  
