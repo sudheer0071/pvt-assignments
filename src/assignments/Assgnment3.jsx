2edde80feae667bfb2a3eec00c5ea0e00076f8a8
sudheer0071 <sudheer1614@gmail.com>
2024-02-20 23:11:04 +0530

    adding typer effect

--- src/assignments/Assgnment3.jsx
+++ src/assignments/Assgnment3.jsx
@@ -5,36 +5,38 @@ export function Assgnment3(){
     
   const [numWords, setNumWords] = useState(0);
   const [words, setWords] = useState([]);
-  // const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [typingEffect, setTypingEffect] = useState('');
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [typingEffect, setTypingEffect] = useState(''); 
 
   useEffect(() => {
     if (words.length === 0) {
       setTypingEffect('');
       return;
     } 
-    let currentWordIndex = 0;
+    // let currentWordIndex = 0;
     const interval = setInterval(() => {
       if (currentWordIndex < words.length) {
         const nextword = words[currentWordIndex]
         setTypingEffect((prev) => prev + ' ' + nextword);
-        currentWordIndex++;
+        setCurrentWordIndex(currentWordIndex+1);
       } else {
         clearInterval(interval);
       }
-    }, 100);
+    }, 100); 
 
     return () => clearInterval(interval);
-  }, [words]);
+  }, [words,currentWordIndex]);
 
   async function generateWords() {
     const response = await axios.get(`https://random-word-api.herokuapp.com/word?number=${numWords}`);
     const words = response.data;
+    const typeWords = words.join(' ') + ' '
+    const letters = typeWords.split('')
     setWords(words);
     setTypingEffect('');
+    setCurrentWordIndex(0)
   }
-
-
+ 
   return (
     <div>
       <input
